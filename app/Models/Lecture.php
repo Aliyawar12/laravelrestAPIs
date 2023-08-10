1623ffe67f0ec9ba076d4bb2494ab49e29eeb613
Aliyawar12 <aliyawar734@gmail.com>
2023-08-10 21:39:44 +0500

    10-08-2023

--- app/Models/Lecture.php
+++ app/Models/Lecture.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Lecture extends Model
 {
     use HasFactory;
-    
-    protected $fillable = ['id','user_id','room_id', 'class_id', 'subject_id', 'day_is', 'timing_id' ];
+
+    protected $fillable = ['id','user_id','room_id', 'class_id', 'subject_id', 'day_id', 'timing_id', 'is_cancelled', 'on_scheduled' ];
 }
